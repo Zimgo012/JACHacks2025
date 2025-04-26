83a565cf9c118c7857a5b91caf653928f85a5d0f
Zimgo012 <rjccabelcina@gmail.com>
2025-04-26 14:55:39 -0400

    feat: initial commit with finished CRUD Animals, and mongoDB

---
+++ backend/controllers/deleteController.js
@@ -0,0 +1,11 @@
+//@desc delete animal by id
+//@route DELETE /animal/id=?
+export const deleteAnimal = (req, res) => {
+
+    try{
+
+
+    }catch(err){
+
+    }
+}
---
+++ backend/controllers/updateController.js
@@ -0,0 +1,3 @@
+//@desc update animal
+//@route UPDATE /animal/id=?
+export const updateAnimal = (req, res) => {}
